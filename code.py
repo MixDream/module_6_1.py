aca296795ce6a76bb8b5b8caead52e37b5818b85
MixDream <max_juldashev@mail.ru>
2024-07-31 16:17:14 +0300

    Update codeee.py

--- code.py
+++ code.py
@@ -1,52 +1,43 @@
 class Animal:
-    alive = True  # Атрибут класса
-    fed = False   # Атрибут класса
-
     def __init__(self, name):
+        self.alive = True
+        self.fed = False
         self.name = name
-
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            Animal.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-
-
-class Mammal(Animal):
-    pass
-
-
-class Predator(Animal):
-    pass
-
-
 class Plant:
     def __init__(self, name):
         self.edible = False
         self.name = name
-
-
+class Mammal(Animal):
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+class Predator(Animal):
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
 class Flower(Plant):
     pass
-
-
 class Fruit(Plant):
     def __init__(self, name):
         super().__init__(name)
         self.edible = True
-
-
-A1 = Predator('пьяный русский медведь')
-A2 = Mammal('Кунг-фу панда')
-P1 = Flower('цветик недоцветик')
-P2 = Fruit('большой персик')
-
-print(A1.name)
-print(P1.name)
-print(A1.alive)
-print(A2.fed)
-A1.eat(P1)
-A2.eat(P2)
-print(A1.alive)
-print(A2.fed)
+a1 = Predator('пьяный русский медведь')
+a2 = Mammal('Кунг-фу панда')
+p1 = Flower('Цветик недоцветик')
+p2 = Fruit('большой персик')
+print(a1.name)
+print(p1.name)
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
